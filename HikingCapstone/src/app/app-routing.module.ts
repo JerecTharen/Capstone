035fb90afd158ac5a0c722c145d27a25b4cbd484
Jacob Bennett <kf7ags@gmail.com>
2019-02-22 11:10:30 -0700

    get auth guard set up

--- HikingCapstone/src/app/app-routing.module.ts
+++ HikingCapstone/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuard} from "./Services/auth.guard";
 
 const routes: Routes = [
   {
@@ -15,8 +16,14 @@ const routes: Routes = [
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
   },
-  { path: 'trail-search', loadChildren: './trail-search/trail-search.module#TrailSearchPageModule' },
-  { path: 'trail-details/:id', loadChildren: './trail-details/trail-details.module#TrailDetailsPageModule' }
+  { path: 'trail-search',
+    loadChildren: './trail-search/trail-search.module#TrailSearchPageModule',
+    canActivate: [AuthGuard]
+  },
+  { path: 'trail-details/:id',
+    loadChildren: './trail-details/trail-details.module#TrailDetailsPageModule',
+    canActivate: [AuthGuard]
+  }
 
 ];
 
