5a0233b2e2a17ff478bb5931f7a58fc182aa5030
Nick <gvmbrtell@gmail.com>
2019-03-07 15:21:26 -0700

    Add sign out

--- HikingCapstone/src/app/app.component.ts
+++ HikingCapstone/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AuthService } from './Services/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -30,7 +32,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private auth: AuthService
   ) {
     this.initializeApp();
   }
@@ -40,6 +43,10 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  signOut() {
+    this.auth.signOut()
   }
 
 }
