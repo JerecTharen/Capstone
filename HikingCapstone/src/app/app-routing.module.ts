9389662c3d3f003f8b9bed92427a48d65ab1aec6
Nick <gvmbrtell@gmail.com>
2019-03-11 20:07:45 -0600

    Make trail-search the root

--- HikingCapstone/src/app/app-routing.module.ts
+++ HikingCapstone/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {AuthGuard} from "./Services/auth.guard";
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'trail-search',
     pathMatch: 'full'
   },
   {
