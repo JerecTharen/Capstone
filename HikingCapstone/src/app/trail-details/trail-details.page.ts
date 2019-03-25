7a86bf69c3d5defba2731beb5c1169f5ed274dee
JerecTharen <kf7ags@gmail.com>
2019-03-24 19:09:25 -0600

    working on add to list buttons

--- HikingCapstone/src/app/trail-details/trail-details.page.ts
+++ HikingCapstone/src/app/trail-details/trail-details.page.ts
@@ -5,6 +5,8 @@ import {Observable} from 'rxjs';
 import {TrailData} from "../Shared/TrailData/trail-data";
 import {WeatherAPIService} from "../Services/weather-api.service";
 import {WeatherGet} from "../Shared/Weather/weather-get";
+import {FirestoreService} from "../Services/firestore/firestore.service";
+import {AuthService} from "../Services/auth.service";
 
 @Component({
   selector: 'app-trail-details',
@@ -16,11 +18,16 @@ export class TrailDetailsPage implements OnInit, OnDestroy {
   private trailID: number = Number(this.route.snapshot.paramMap.get('id'));
   private trailData: Observable<TrailData> = this.hikingService.getTrailDetails(this.trailID);
   private weatherData: Observable<WeatherGet>;
+  private hasHiked;
+  private notHiked;
+  private loggedIn: boolean;
 
   constructor(
       private hikingService: HikingAPIService,
       private route: ActivatedRoute,
-      private weatherService: WeatherAPIService
+      private weatherService: WeatherAPIService,
+      private firestore: FirestoreService,
+      private auth: AuthService,
   ) { }
 
   ngOnInit(): void {
@@ -28,6 +35,14 @@ export class TrailDetailsPage implements OnInit, OnDestroy {
     this.hikingService.getTrailDetails(this.trailID).subscribe((trailsData)=>{
       // console.log(trailsData);
       this.weatherData = this.weatherService.getWeatherForLatLong(trailsData.trails[0].latitude, trailsData.trails[0].longitude);
+    });
+
+    this.auth.authed().subscribe((loggedIn)=>{
+      this.loggedIn = loggedIn;
+      if(this.loggedIn){
+        this.hasHiked = this.firestore.getCompleted();
+        this.notHiked = this.firestore.getInterested();
+      }
     });
   }
   ngOnDestroy(): void {
