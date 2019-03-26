df2b4e43175241fb3d8ef1d8f1bfe6c182b854a0
JerecTharen <kf7ags@gmail.com>
2019-03-25 19:09:51 -0600

    Added some styling andebugging

--- HikingCapstone/src/app/trail-details/trail-details.page.ts
+++ HikingCapstone/src/app/trail-details/trail-details.page.ts
@@ -13,113 +13,115 @@ import {ForcastGet} from "../Shared/Weather/forcast-get";
 import {ForcastListObject} from "../Shared/Weather/forcast-list-object";
 
 @Component({
-  selector: 'app-trail-details',
-  templateUrl: './trail-details.page.html',
-  styleUrls: ['./trail-details.page.scss'],
+    selector: 'app-trail-details',
+    templateUrl: './trail-details.page.html',
+    styleUrls: ['./trail-details.page.scss'],
 })
 export class TrailDetailsPage implements OnInit, OnDestroy {
 
-  private trailID: number = Number(this.route.snapshot.paramMap.get('id'));
-  private trailData: Observable<TrailData> = this.hikingService.getTrailDetails(this.trailID);
-  private weatherData: Observable<WeatherGet>;
-  private weatherForcast: Observable<ForcastGet>;
-  private fiveDay: ForcastListObject[] = [];
-  private hasHikedObs;
-  private notHikedObs;
-  private hikedBool: boolean = false;
-  private notHikedBool: boolean = false;
-  private loggedIn: boolean;
-  private unsubscribe$ = new Subject();
+    private trailID: number = Number(this.route.snapshot.paramMap.get('id'));
+    private trailData: Observable<TrailData> = this.hikingService.getTrailDetails(this.trailID);
+    private weatherData: Observable<WeatherGet>;
+    private weatherForcast: Observable<ForcastGet>;
+    private fiveDay: ForcastListObject[] = [];
+    private hasHikedObs;
+    private notHikedObs;
+    private hikedBool: boolean = false;
+    private notHikedBool: boolean = false;
+    private loggedIn: boolean;
+    private unsubscribe$ = new Subject();
 
-  constructor(
-      private hikingService: HikingAPIService,
-      private route: ActivatedRoute,
-      private weatherService: WeatherAPIService,
-      private firestore: FirestoreService,
-      private auth: AuthService,
-  ) { }
+    constructor(
+        private hikingService: HikingAPIService,
+        private route: ActivatedRoute,
+        private weatherService: WeatherAPIService,
+        private firestore: FirestoreService,
+        private auth: AuthService,
+    ) { }
 
-  ngOnInit(): void {
-    // Math.round(4.7);
-    this.hikingService.getTrailDetails(this.trailID).subscribe((trailsData)=>{
-      // console.log(trailsData);
-      this.weatherData = this.weatherService.getWeatherForLatLong(trailsData.trails[0].latitude, trailsData.trails[0].longitude);
-      this.weatherForcast = this.weatherService.getWeatherForcast(trailsData.trails[0].latitude, trailsData.trails[0].longitude);
-      this.weatherForcast.subscribe((forcastData: ForcastGet)=>{
-        for(let i: number = 0; i < forcastData.list.length; i++){
-          if(i === 8 || i === 16 || i === 32 || i === 39){
-            this.fiveDay.push(forcastData.list[i]);
-          }
-        }
-      });
-    });
+    ngOnInit(): void {
+        // Math.round(4.7);
+        this.hikingService.getTrailDetails(this.trailID).subscribe((trailsData)=>{
+            // console.log(trailsData);
+            this.weatherData = this.weatherService.getWeatherForLatLong(trailsData.trails[0].latitude, trailsData.trails[0].longitude);
+            this.weatherForcast = this.weatherService.getWeatherForcast(trailsData.trails[0].latitude, trailsData.trails[0].longitude);
+            this.weatherForcast.subscribe((forcastData: ForcastGet)=>{
+                // console.log(forcastData);
+                for(let i: number = 0; i < forcastData.list.length; i++){
+                    if(i===0 || i === 8 || i === 16 || i === 32 || i === forcastData.list.length -1){
+                        this.fiveDay.push(forcastData.list[i]);
+                        // console.log(this.fiveDay);
+                    }
+                }
+            });
+        });
 
-    this.auth.authed().subscribe((loggedIn)=>{
-      // console.log('in logged in block');
-      this.loggedIn = loggedIn;
-      if(this.loggedIn){
-        // console.log('Logged In True');
-        this.hasHikedObs = this.firestore.getCompleted().pipe(
-          takeUntil(this.unsubscribe$),
-          tap((hikedList: DBTrailData[]) => {
-            let found = false;
-            for(let i: number = 0; i < hikedList.length; i++){
-                // console.log(hikedList);
-                // console.log(`Checking ${hikedList[i].id} against ${this.trailID}`);
-              if(hikedList[i].id === this.trailID){
-                found = true;
-              }
+        this.auth.authed().subscribe((loggedIn)=>{
+            // console.log('in logged in block');
+            this.loggedIn = loggedIn;
+            if(this.loggedIn){
+                // console.log('Logged In True');
+                this.hasHikedObs = this.firestore.getCompleted().pipe(
+                    takeUntil(this.unsubscribe$),
+                    tap((hikedList: DBTrailData[]) => {
+                        let found = false;
+                        for(let i: number = 0; i < hikedList.length; i++){
+                            // console.log(hikedList);
+                            // console.log(`Checking ${hikedList[i].id} against ${this.trailID}`);
+                            if(hikedList[i].id === this.trailID){
+                                found = true;
+                            }
+                        }
+                        if(found){
+                            this.hikedBool = true;
+                        }
+                    })
+                ).subscribe();
+                this.notHikedObs = this.firestore.getInterested().pipe(
+                    takeUntil(this.unsubscribe$),
+                    tap((notHikedList: DBTrailData[])=>{
+                        // console.log('In Want to Hike Block');
+                        let found = false;
+                        for(let i: number = 0; i < notHikedList.length; i++){
+                            // console.log(`Checking ${notHikedList[i].id} against ${this.trailID}`);
+                            if(notHikedList[i].id === this.trailID){
+                                found = true;
+                            }
+                        }
+                        if(found){
+                            this.notHikedBool = true;
+                        }
+                    })
+                ).subscribe();
             }
-            if(found){
-              this.hikedBool = true;
-            }
-          })
-        ).subscribe();
-        this.notHikedObs = this.firestore.getInterested().pipe(
-          takeUntil(this.unsubscribe$),
-          tap((notHikedList: DBTrailData[])=>{
-              // console.log('In Want to Hike Block');
-            let found = false;
-            for(let i: number = 0; i < notHikedList.length; i++){
-                // console.log(`Checking ${notHikedList[i].id} against ${this.trailID}`);
-              if(notHikedList[i].id === this.trailID){
-                found = true;
-              }
-            }
-            if(found){
-              this.notHikedBool = true;
-            }
-          })
-        ).subscribe();
-      }
-    });
-  }
-  ngOnDestroy(): void {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
-
-  getWeatherData(lat: number, long: number): void{
-    console.log('ran get');
-    this. weatherData = this.weatherService.getWeatherForLatLong(lat, long);
-  }
-
-  addToHiked(): void{
-    console.log('adding to hiked');
-    if(this.notHikedBool){
-      this.firestore.removeFromInterested(this.trailID);
+        });
     }
-    this.firestore.addToCompleted(this.trailID);
-  }
+    ngOnDestroy(): void {
+        this.unsubscribe$.next();
+        this.unsubscribe$.complete();
+    }
 
-  addToWantToHike(): void{
-    console.log('adding to want to hike');
-    this.firestore.addToInterested(this.trailID);
-  }
+    getWeatherData(lat: number, long: number): void{
+        console.log('ran get');
+        this. weatherData = this.weatherService.getWeatherForLatLong(lat, long);
+    }
 
-  devReset(): void{
-    this.firestore.removeFromCompleted(this.trailID);
-    this.firestore.removeFromInterested(this.trailID);
-  }
+    addToHiked(): void{
+        console.log('adding to hiked');
+        if(this.notHikedBool){
+            this.firestore.removeFromInterested(this.trailID);
+        }
+        this.firestore.addToCompleted(this.trailID);
+    }
+
+    addToWantToHike(): void{
+        console.log('adding to want to hike');
+        this.firestore.addToInterested(this.trailID);
+    }
+
+    devReset(): void{
+        this.firestore.removeFromCompleted(this.trailID);
+        this.firestore.removeFromInterested(this.trailID);
+    }
 
 }
