3f0c1c626ac63ed2febbfcebff4d9bde45699a87
Nick <gvmbrtell@gmail.com>
2019-05-12 20:56:29 -0600

    My trails tabs

--- HikingCapstone/src/app/my-trails/my-trails.page.ts
+++ HikingCapstone/src/app/my-trails/my-trails.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-trails.page.scss'],
 })
 export class MyTrailsPage implements OnInit {
+  tab: string = 'hiked';
 
   constructor() { }
 
