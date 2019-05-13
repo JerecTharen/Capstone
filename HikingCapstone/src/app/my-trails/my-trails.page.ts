cbf5a3f22e58b9a185ed1ef46700b9e350c6925d
Nick <gvmbrtell@gmail.com>
2019-05-12 21:25:32 -0600

    Trail pages

--- HikingCapstone/src/app/my-trails/my-trails.page.ts
+++ HikingCapstone/src/app/my-trails/my-trails.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../Services/firestore/firestore.service';
+import { Observable } from 'rxjs';
+import { DBTrailData } from '../Shared/DB/db-trail-data';
 
 @Component({
   selector: 'app-my-trails',
@@ -8,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 export class MyTrailsPage implements OnInit {
   tab: string = 'hiked';
 
-  constructor() { }
+  $hiked: Observable<DBTrailData[]>;
+  $interested: Observable<DBTrailData[]>;
+
+  constructor(
+    private fs: FirestoreService
+  ) { }
 
   ngOnInit() {
+    this.$hiked = this.fs.getCompleted();
+    this.$interested = this.fs.getInterested();
   }
 
 }
